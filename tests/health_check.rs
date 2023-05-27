7271f22966ca25a18b0017bf092c2b33f0c21e73
ydrive65 <ydrive65@yahoo.com>
2023-05-26 22:22:17 -0400

    only started 3.7.2

---
+++ tests/health_check.rs
@@ -0,0 +1,47 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{address}/health_check"))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .post(&format!("{address}/subscribe"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body("name=le%20guin&email=ursula_le_guin%40gmail.com")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(200, response.status().as_u16());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{port}")
+}
