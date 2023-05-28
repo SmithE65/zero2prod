af41bd7589543c55f350507b3c8640c165573297
ydrive65 <ydrive65@yahoo.com>
2023-05-28 12:10:09 -0400

    finishing 3.7

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -38,6 +38,40 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=l2%20guin", "email missing from request"),
+        (
+            "email=ursula_le_guin%40gmail.com",
+            "name missing from request",
+        ),
+        ("", "no data in request"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(format!("{address}/subscribe"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to make request");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Expected bad request when {error_message}"
+        );
+    }
+}
+
+#[allow(clippy::let_underscore_future)]
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address.");
     let port = listener.local_addr().unwrap().port();
