c73b9c6971aa2345d10f6d9f866d049ca1e5e001
ydrive65 <ydrive65@yahoo.com>
2023-05-29 21:50:00 -0400

    Starting into logging...

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Unable to read configuration file.");
+
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to database.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
+
     run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,15 +2,16 @@ use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::web;
-use actix_web::App;
-use actix_web::HttpServer;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
+
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(connection.clone())
             .route("/health_check", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
