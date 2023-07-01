a73edb37fa278fd353c9d3219c2db4c58d4f0c02
ydrive65 <ydrive65@yahoo.com>
2023-07-01 13:20:48 -0400

    adding send_email

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::{
     configuration::get_configuration,
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -18,11 +19,25 @@ async fn main() -> Result<(), std::io::Error> {
         .await
         .expect("Failed to connect to database.");
 
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,24 @@
 use std::net::TcpListener;
 
+use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    connection: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
 
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .app_data(connection.clone())
+            .app_data(email_client.clone())
             .route("/health_check", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
     })
