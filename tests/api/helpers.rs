ea4a0dffd339f46a24cfc230f5fbd5465c166c88
ydrive65 <ydrive65@yahoo.com>
2023-08-20 16:23:22 -0400

    adding basic auth

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -45,8 +45,10 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        let (username, password) = self.test_user().await;
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
+            .basic_auth(username, Some(password))
             .json(&body)
             .send()
             .await
@@ -75,6 +77,14 @@ impl TestApp {
             plain_text: text_link,
         }
     }
+
+    pub async fn test_user(&self) -> (String, String) {
+        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1",)
+            .fetch_one(&self.db_pool)
+            .await
+            .expect("Failed to retrieve test user");
+        (row.username, row.password)
+    }
 }
 
 #[allow(clippy::let_underscore_future)]
@@ -101,12 +111,27 @@ pub async fn spawn_app() -> TestApp {
     let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
 
-    TestApp {
+    let test_app = TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         port: application_port,
-    }
+    };
+    add_test_user(&test_app.db_pool).await;
+    test_app
+}
+
+async fn add_test_user(pool: &PgPool) {
+    sqlx::query!(
+        "INSERT INTO users (user_id, username, password)
+        VALUES ($1, $2, $3)",
+        Uuid::new_v4(),
+        Uuid::new_v4().to_string(),
+        Uuid::new_v4().to_string(),
+    )
+    .execute(pool)
+    .await
+    .expect("Failed to create test user");
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
